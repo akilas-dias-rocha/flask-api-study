9e565b93eff040512cdeaa44d4fa364f5c207788
Akilas Dias <akilas.rocha@indicium.tech>
2026-07-08 15:30:04 -0300

    feat: Initialize Flask application with SQLAlchemy and basic routing

---
+++ main.py
@@ -0,0 +1,35 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+
+# Database
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///travel.db"
+db = SQLAlchemy(app)
+
+class Destination(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    destination = db.Column(db.String(50), nullable=False)
+    country = db.Column(db.String(50), nullable=False)
+    rating = db.Column(db.Float, nullable=False)
+
+    def to_dict(self):
+        return {
+            "id" : self.id,
+            "destination" : self.destination,
+            "country" : self.country,
+            "rating" : self.rating
+        }
+
+with app.app_context():
+    db.create_all()
+
+# Routes
+@app.route("/")
+def home():
+    return "Hello World"
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
