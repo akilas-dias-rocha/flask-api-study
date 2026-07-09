6206799cd8740a9fef962824c61e66cf0444ecc9
Akilas Dias <akilas.rocha@indicium.tech>
2026-07-09 16:35:09 -0300

    feat: Add PUT and DELETE methods for destination management

--- main.py
+++ main.py
@@ -58,6 +58,34 @@ def add_destination():
 
     return jsonify(new_destination.to_dict()), 201
 
+# PUT
+@app.route("/destinations/<int:destination_id>", methods=["PUT"])
+def update_destination(destination_id):
+    data = request.get_json()
+
+    destination = Destination.query.get(destination_id)
+    if destination:
+        destination.destination = data.get("destination", destination.destination)
+        destination.country = data.get("country", destination.country)
+        destination.rating = data.get("rating", destination.rating)
+
+        db.session.commit()
+        return jsonify(destination.to_dict())
+    else:
+        return jsonify({"error":"Destination not found!"}), 404
+
+# DELETE
+@app.route("/destinations/<int:destination_id>", methods=["DELETE"])
+def delete_destination(destination_id):
+    destination = Destination.query.get(destination_id)
+    if destination:
+        db.session.delete(destination)
+        db.session.commit()
+
+        return jsonify({"message":"Destination was deleted!"})
+    else:
+        return  jsonify({"error":"Destination not found!"}), 404
+
 
 
 if __name__ == "__main__":
